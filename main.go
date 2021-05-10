786b5133f15771c30901bb277f93a09147bc1255
Iñigo <igponce@gmail.com>
2021-05-10 14:22:38 +0200

    Calcula tarifa en peninsula y baleares

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"time"
 
 	"github.com/getlantern/systray"
 )
@@ -28,3 +29,24 @@ func onExit() {
    log.Print("Saliendo")
 } 
 
+func tarifaActual () int {
+
+    var tarifa int
+    var tarifa_pen[] = []int{ 1, 1, 1, 1, 1, 1, 1, 1,  // 0:00 - 7:59   - Valle
+	   		      2, 2,                    // 8:00 - 9:59   - Llana
+			      3, 3, 3, 3,              // 10:00 - 13:59 - Punta
+			      2, 2, 2, 2,              // 14:00 - 17:59 - Llana
+			      3, 3, 3, 3,              // 18:00 - 21:59 - Punta
+			      2, 2,                    // 22:00 - 23:59 - Llana 
+			    }
+    var tarifas []string = []string{"undef","valle","llana","punta"}
+    ahora := time.Now()
+    dd, mm, yy := ahora.Date()
+    hh, mm, ss := ahora.Clock()
+    weekday := ahora.Weekday()
+	
+    tarifa := tarifa_pen[hh]
+    fmt.Printf("%d%0d%0dT%0d:%0d:%0d - Tarifa %s", yy,mm, dd, hh, mm, ss)
+
+    return tarifa
+}
